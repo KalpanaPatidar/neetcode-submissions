757c6633dffcefa23e9d3098a00624ca977f695b
KalpanaPatidar <163427005+KalpanaPatidar@users.noreply.github.com>
2026-08-24 16:30:45 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool validWordAbbreviation(string word, string abbr) {
+        int i=0;
+        int j=0;
+        int n=word.length();
+        int m=abbr.length();
+
+        while(i<n && j<m){
+            if(abbr[j]=='0'){
+                return false;
+
+            }
+            if(isdigit(abbr[j])){
+                int num=0;
+                while(j < m && isdigit(abbr[j])){
+                    num=num*10 +(abbr[j]-'0');
+                    j++;
+                }
+                i += num;
+            }
+            else{
+                if(word[i]!=abbr[j]){
+                    return false;
+                }
+                i++;
+                j++;
+            }
+        }
+        return i == n && j == m;
+    }
+};          
